54becfee4b016b4aaa210afe0ee82a6a3dc65210
gurbanovcavad <gurbanovcavad7@gmail.com>
2026-05-21 12:14:35 +0400

    add Singleton Pattern example

---
+++ src/main/java/org/example/Creational/Singleton.java
@@ -0,0 +1,26 @@
+package org.example.Creational;
+
+class Logger {
+    private static Logger instance = null;
+
+    private Logger() {
+
+    }
+
+    public static Logger getInstance() {
+        instance = ((instance == null) ? new Logger() : instance);
+        return instance;
+    }
+
+    public void test() {
+        System.out.println("This is an instance of Logger class");
+    }
+}
+
+public class Singleton {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+
+        logger.test();
+    }
+}
